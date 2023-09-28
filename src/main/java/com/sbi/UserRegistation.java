5fe0be2d86d0c87b3e3bfff8710a05540df4b949
sanjay18121999 <biswalsanjay1812@gmail.com>
2023-09-28 11:41:37 +0530

    first commit

---
+++ src/main/java/com/sbi/UserRegistation.java
@@ -0,0 +1,15 @@
+package com.sbi;
+
+import in.sanjay.security.Pwdsecurityservice;
+
+public class UserRegistation {
+
+	public static void main(String[] args) {
+		
+		Pwdsecurityservice pss = new Pwdsecurityservice();
+		 String encode = pss.encord("sanjay");
+		 System.out.println(encode);
+		 
+	}
+
+}
