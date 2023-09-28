97c435497864fe5a46bcd0a7ecccd23892d4e172
Chinu <chinmaysahoo550550@gmail.com>
2023-09-28 22:31:54 +0530

    updated

--- src/main/java/com/sbi/UserRegistation.java
+++ src/main/java/com/sbi/UserRegistation.java
@@ -7,11 +7,9 @@ public class UserRegistation {
 	public static void main(String[] args) {
 		
 		Pwdsecurityservice pss = new Pwdsecurityservice();
-<<<<<<< HEAD
+
 		 String encode = pss.encord("sanjay biswal65");
-=======
-		 String encode = pss.encord("sanjay biswal2");
->>>>>>> fa970231841d3cf3c271d4c37f97b34fd1b93a48
+
 		 System.out.println(encode);
 		 
 	}
